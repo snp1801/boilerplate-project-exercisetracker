2ac02298e8e1a72b13a70c272935b4424583d95a
snp1801 <snp1801@gmail.com>
2023-05-05 21:34:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sam1811harper:ila!1801@cluster0.2wrrnxn.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+require('dotenv').config()
+
+var Schema = mongoose.Schema
+const userSchema = new Schema({
+  userName: {
+    type : String,
+    required : true
+  }
+});
+
+var User = mongoose.model('User', userSchema)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get("/test", (req, res) => {
+  res.json({greet : "Working"})
+})
+
+app.post('/api/users', (req,res) => {
+  // const user = req.body.username
+  // console.log(user)
+  var curr = new User({ userName : req.body.username });
+  curr.save((err, data) => {
+    if (err) return res.json({error: err})
+    // console.log(data)
+    res.json({userName: data.userName, _id: data._id})
+  })
+})
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: err });
+    }
+    return res.json(users);
+  });
+});
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
