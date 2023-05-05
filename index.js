ab6dd7cacc2c4ec7699f43c830e688ec403b5079
snp1801 <snp1801@gmail.com>
2023-05-06 03:35:36 +0530

    post /api/users/:id/exercises

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://sam1811harper:ila!1801@cluster0.2wrrnxn.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 require('dotenv').config()
 
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const monthsOfYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 var Schema = mongoose.Schema
 const userSchema = new Schema({
   userName: {
@@ -13,7 +16,20 @@ const userSchema = new Schema({
   }
 });
 
+const logSchema = new Schema({
+  description: {
+    type: String, required: true
+  },
+  duration : {
+    type: Number, required: true
+  },
+  date: {
+    type : String
+  }
+});
+
 var User = mongoose.model('User', userSchema)
+var Log = mongoose.model('log', logSchema)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -30,8 +46,8 @@ app.get("/test", (req, res) => {
 app.post('/api/users', (req,res) => {
   // const user = req.body.username
   // console.log(user)
-  var curr = new User({ userName : req.body.username });
-  curr.save((err, data) => {
+  var curr_user = new User({ userName : req.body.username });
+  curr_user.save((err, data) => {
     if (err) return res.json({error: err})
     // console.log(data)
     res.json({userName: data.userName, _id: data._id})
@@ -46,6 +62,34 @@ app.get('/api/users', (req, res) => {
     return res.json(users);
   });
 });
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { description, duration, date = new Date() } = req.body;
+  const id = req.params._id;
+  const new_date = new Date(date)
+  const dayOfWeek = daysOfWeek[new_date.getDay()];
+  const month = monthsOfYear[new_date.getMonth()];
+  const dayOfMonth = new_date.getDate();
+  const year = new_date.getFullYear();
+  const formattedDate = `${dayOfWeek} ${month} ${dayOfMonth} ${year}`;
+  User.findById(id, (err, user) => {
+    // const user_n = user.userName;
+    if (err) return console.log(err);
+    var arr = { "description": description, "duration": duration, "date": formattedDate }
+    const curr_log = new Log( arr )
+    // console.log(curr_log);
+    curr_log.save((err, data) => {
+      if (err) return res.json({error: err})
+      // data["_id"] = id;
+      // data["userName"] = user.userName;
+      // console.log(data)
+      // res.json(data)
+    })
+    console.log(user)
+    arr["_id"] = id;
+    arr["userName"] = user.userName;
+    res.json(arr)
+  })
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
